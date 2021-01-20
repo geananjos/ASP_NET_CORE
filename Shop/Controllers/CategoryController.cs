4e68eb630999f341594c4c639d9f295aa6699328
Gean Anjos <geandevelopments@outlook.com>
2021-01-19 23:54:56 -0300

    Commit inicial

---
+++ Shop/Controllers/CategoryController.cs
@@ -0,0 +1,116 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shop.Data;
+using Shop.Models;
+
+namespace Shop.Controllers
+{
+    [Route("categories")]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public async Task<ActionResult<List<Category>>> Get([FromServices]DataContext context)
+        {
+            var categories = await context.Categories.AsNoTracking().ToListAsync();
+            return categories;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult<Category>> GetById
+        (
+            int id,
+            [FromServices]DataContext context
+        )
+        {
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+            
+            try
+            {
+                var category = await context.Categories.AsNoTracking().FirstOrDefaultAsync(c => c.Id == id);
+                return Ok(category);
+            }
+            catch (Exception)
+            {
+                return BadRequest(new {message = "Categoria não encontrada"});
+            }
+        }
+
+        [HttpPost]
+        [Route("")]
+        public async Task<ActionResult<List<Category>>> Post
+        (
+            [FromBody]Category model,
+            [FromServices]DataContext context
+        )
+        {
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+            
+            try
+            {
+                context.Categories.Add(model);
+                await context.SaveChangesAsync();
+                return Ok(model);    
+            }
+            catch
+            {
+                return BadRequest(new { message = "Não foi possível criar a categoria"});
+            }
+            
+        }
+
+        [HttpPut]
+        [Route("{id:int}")]
+        public async Task<ActionResult<List<Category>>> Put
+        (
+            int id, 
+            [FromBody]Category model,
+            [FromServices]DataContext context
+        )
+        {
+            if(model.Id == id)
+                return Ok(model);
+
+            try
+            {
+               context.Entry<Category>(model).State = EntityState.Modified;
+               await context.SaveChangesAsync();
+               return Ok(model);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return BadRequest(new {messsage = "Não foi possível atualizar a categoria."});
+            }
+        }
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        public async Task<ActionResult<List<Category>>> Delete
+        (
+            int id,
+            [FromServices]DataContext context
+        )
+        {
+            var category = await context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+                if(category == null)
+                    return NotFound(new {message = "Categoria não encontrada"});
+
+            try
+            {
+                context.Categories.Remove(category);
+                await context.SaveChangesAsync();
+                return Ok(category);
+            }
+            catch(Exception)
+            {
+                return BadRequest(new {message = "Não foi possível remover a categoria"});
+            }   
+        }
+    }
+}
---
+++ Shop/Settings.cs
@@ -0,0 +1,7 @@
+namespace Shop
+{
+    public static class Settings
+    {
+        public static string Secret = "65d465e207004e7d9d9a6915513ff89b";
+    }
+}
